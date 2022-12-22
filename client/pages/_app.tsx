81fae74c1115b7113bdce0e7763a75394a039859
Mohammad Mizanur Rahman <58907522+MOHAMMADmiZAN@users.noreply.github.com>
2022-12-22 09:41:38 +0600

    react query init , matarial ui, form hook

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,22 +1,31 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import {useStoreRehydrated,StoreProvider} from "easy-peasy";
+import type {AppProps} from 'next/app'
+import {StoreProvider, useStoreRehydrated} from "easy-peasy";
 import store from "../store/store";
+import {QueryClient} from "@tanstack/query-core";
+import {QueryClientProvider,} from "@tanstack/react-query";
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 
-export default function App({ Component, pageProps }: AppProps) {
-  // @ts-ignore
-  function WaitForStateRehydration ({children}) {
+const queryClient = new QueryClient()
+
+// @ts-ignore
+function WaitForStateRehydration({children}) {
     const isRehydrated = useStoreRehydrated()
     return isRehydrated ? children : null
-  }
+}
 
-  return(
-      <StoreProvider store={store}>
-        <WaitForStateRehydration>
-          <Component {...pageProps} />
-        </WaitForStateRehydration>
-      </StoreProvider>
-  )
+export default function App({Component, pageProps}: AppProps) {
+
+    return (
+        <StoreProvider store={store}>
+            <WaitForStateRehydration>
+                <QueryClientProvider client={queryClient}>
+                    <Component {...pageProps} />
+                    <ReactQueryDevtools initialIsOpen={false}/>
+                </QueryClientProvider>
+            </WaitForStateRehydration>
+        </StoreProvider>
+    )
 
 
 }
