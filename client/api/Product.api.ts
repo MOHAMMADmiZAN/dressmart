7536f9b7402425f847d285fd32f4a7d034166366
Mohammad Mizanur Rahman <58907522+MOHAMMADmiZAN@users.noreply.github.com>
2022-12-27 21:12:28 +0600

    product Data refactor

--- client/api/Product.api.ts
+++ client/api/Product.api.ts
@@ -2,8 +2,40 @@
 import {ProductApi} from "./api";
 
 
-
 export const fetchProducts = async () => {
     const res = await ProductApi.get("?populate[variants][populate][0]=thumbnail");
-    return res.data;
+    return res.data.data.reduce((acc: any, cur: any) => {
+        const {id, attributes} = cur;
+
+        const variantData = attributes.variants.data.reduce((acc: any, cur: any) => {
+            const {id, attributes} = cur;
+            const imageData = attributes.thumbnail.data.reduce((acc: any, cur: any) => {
+                const {id, attributes} = cur;
+                acc['src'] = {
+                    id,
+                    src: attributes.url,
+                }
+                return acc;
+
+            }, {})
+
+            acc[attributes.color] = {
+                id,
+                value: attributes.color,
+                description: attributes.description,
+                regularPrice: attributes.regular_price,
+                salePrice: attributes.sale_price,
+                stock: attributes.stock_quantity,
+                image: imageData
+
+
+            };
+            return acc;
+
+
+        }, {})
+        acc.push({id, name: attributes.name, variants: variantData});
+        return acc;
+
+    }, []);
 }
--- client/components/Organisms/Products/ByRecentProduct/ByRecentProduct.tsx
+++ client/components/Organisms/Products/ByRecentProduct/ByRecentProduct.tsx
@@ -6,10 +6,8 @@ function ByRecentProduct() {
     useEffect(() => {
         // fetch Products
         fetchProducts().then((res) => {
-            res.data.reduce((acc:Array<object>, cur:Array<object>) => {
-                console.log(acc, cur);
+            console.log(res);
 
-            },[])
         })
 
     })
