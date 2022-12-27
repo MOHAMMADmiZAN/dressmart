d4c2e66034d93e3d48b6d9deeb82b1ffc80340f7
Mohammad Mizanur Rahman <58907522+MOHAMMADmiZAN@users.noreply.github.com>
2022-12-27 22:06:18 +0600

    product Data refactor

--- client/api/Product.api.ts
+++ client/api/Product.api.ts
@@ -1,7 +1,40 @@
 // product fetch api
 import {ProductApi} from "./api";
 
+type Product = {
+    id: number;
+    name: string;
+    variants: VariantArray
 
+}
+
+
+type variant = {
+    id: number;
+    value: string;
+    description: string;
+    regularPrice: number;
+    salePrice: number;
+    stock: string;
+    image: image;
+}
+type image = {
+    src: src;
+}
+type src = {
+    url: string;
+    id: number;
+}
+
+export interface productArray extends Array<Product> {
+
+}
+
+export interface VariantArray extends Array<variant> {
+
+}
+
+// recent products fetch api
 export const fetchProducts = async () => {
     const res = await ProductApi.get("?populate[variants][populate][0]=thumbnail");
     return res.data.data.reduce((acc: any, cur: any) => {
@@ -18,8 +51,7 @@ export const fetchProducts = async () => {
                 return acc;
 
             }, {})
-
-            acc[attributes.color] = {
+            acc.push({
                 id,
                 value: attributes.color,
                 description: attributes.description,
@@ -29,11 +61,11 @@ export const fetchProducts = async () => {
                 image: imageData
 
 
-            };
+            })
             return acc;
 
 
-        }, {})
+        }, [])
         acc.push({id, name: attributes.name, variants: variantData});
         return acc;
 
