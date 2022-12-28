ad2c472e7487b7d4533855e6d014d86476a4aba4
Alsadi30 <abdullahsadi39@gmail.com>
2022-12-29 00:47:08 +0600

    cart Drawer added

--- client/components/Molecules/Shared/CartBadge/CartBadge.tsx
+++ client/components/Molecules/Shared/CartBadge/CartBadge.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
-import {Badge, Button} from "@mui/material";
+import { Badge, Button } from "@mui/material";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Cart from '../../../Organisms/Cart/Cart';
+
 
 function CartBadge(): JSX.Element {
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        console.log(open)
+        console.log('da')
+        setOpen(true);
+        console.log(open)
+    };
+
+    const handleDrawerClose = () => {
+        console.log('clse')
+        setOpen(false)
+        console.log(open)
+    };
+
     return (
         <>
 
-            <Button>
+            <Button onClick={handleDrawerOpen}>
                 <Badge badgeContent={0} color={`error`} showZero={true}>
-                    <ShoppingCartIcon sx={{color: '#D4AF37'}}/>
+                    <ShoppingCartIcon sx={{ color: '#D4AF37' }} />
                 </Badge>
+
             </Button>
+            <Cart handleDrawerClose={handleDrawerClose} open={open} />
 
         </>
     );
---
+++ client/components/Organisms/Cart/Cart.tsx
@@ -0,0 +1,56 @@
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import { styled, useTheme } from '@mui/material/styles';
+
+const drawerWidth = 340;
+
+
+
+
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+
+    // necessary for content to be below app bar
+
+}));
+
+type AppPros = {
+    open: boolean
+    handleDrawerClose: any
+}
+
+export default function Cart({ open, handleDrawerClose }: AppPros) {
+    const theme = useTheme();
+
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                    },
+                }}
+                variant="persistent"
+                anchor="right"
+                open={open}
+            >
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose} >
+                        {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </DrawerHeader>
+
+
+            </Drawer>
+        </Box >
+    );
+}
