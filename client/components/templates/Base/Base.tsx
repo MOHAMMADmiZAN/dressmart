aceb8af23b3e539a01bb1ee21106e1d3a006840f
Mohammad Mizanur Rahman <58907522+MOHAMMADmiZAN@users.noreply.github.com>
2023-01-08 03:17:39 +0600

    Base

--- client/components/templates/Base/Base.tsx
+++ client/components/templates/Base/Base.tsx
@@ -2,14 +2,15 @@ import React, {memo} from 'react';
 import Header from "../../Organisms/Header/Header";
 import Footer from "../../Organisms/Footer/Footer";
 import {Box} from "@mui/material";
+import {NextPage} from "next";
 
 interface BaseProps {
-    inner?: React.ReactNode[];
+    inner?: Array<React.ReactNode>;
     children?: React.ReactNode;
 }
 
-function Base(BaseProps: BaseProps): JSX.Element {
-    const {inner, children} = BaseProps;
+const Base: NextPage<BaseProps> = ({inner, children})=> {
+
     return (
         <>
             <Header/>
